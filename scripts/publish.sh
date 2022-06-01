c89a03c3649745f4f79c93087553e065f0a46485
dockersurface <1011273803@qq.com>
2022-06-01 22:07:14 +0800

    feat: publish npm actions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,17 +5,7 @@ set -e
 pnpm i --frozen-lockfile
 pnpm update:version
 
-pnpm build
-
-cd dist/element-plus
-npm publish
-cd -
-
-cd internal/eslint-config
-npm publish
-cd -
-
-cd internal/metadata
+cd packages/components
 pnpm build
 npm publish
 cd -
